2cb376b683d56e3e66f463753dedd6e87f21a55f
Danish Juneja <innovativechamp123@gmail.com>
2025-08-09 22:19:16 +0530

    change

--- june6app/models.py
+++ june6app/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length= 12)
     email = models.CharField(max_length= 45)
     desc = models.TextField(max_length= 122)
-    sign= models.FileField(upload_to='media/', blank = True,null = True)
+    sign= models.FileField(upload_to='', blank = True,null = True)
     hear = models.TextField(max_length= 122,null = True)
 
     def __str__(self):
--- june6proj/settings.py
+++ june6proj/settings.py
@@ -95,7 +95,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Added for Render
 
 # Media files
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
